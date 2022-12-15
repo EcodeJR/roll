4192e0f413615c8a3c37701d8b6a2c15e0426761
Emmanuel Dalyop <108156886+EcodeJR@users.noreply.github.com>
2022-12-15 12:51:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+let main_T = document.getElementById('main_T');
+let image = document.getElementById('image');
+let bike = document.getElementById('bike');
+let car = document.getElementById('car');
+let plane = document.getElementById('plane');
+let jet_ski = document.getElementById('jet_ski');
+let index = 0;
+
+let left = document.getElementById('left');
+let right = document.getElementById('right');
+
+
+let ImgList = [{
+        text: 'CYCLING MODE',
+        main_img: '/red-sports-bike.png',
+        dot: bike
+    }, {
+        text: 'DRIVINNG MODE',
+        main_img: '/red-sports-bike.png',
+        dot: car
+    },
+    {
+        text: 'FLYING MODE',
+        main_img: '/red-sports-bike.png',
+        dot: plane
+    },
+    {
+        text: 'SKIING MODE',
+        main_img: '/red-sports-bike.png',
+        dot: jet_ski
+    }
+];
+
+function loadContent(index) {
+    main_T.innerHTML = ImgList[index].text;
+    image.setAttribute('src', ImgList[index].main_img);
+    if (ImgList[index]) {
+        ImgList[index].dot.style.backgroundColor = '#fff';
+    } else {
+        ImgList[index].dot.style.backgroundColor = '#0f1414';
+    }
+
+}
+loadContent(index);
+
+//prev content
+function prev_content() {
+    if (index > 0) {
+        index -= 1;
+        loadContent(index);
+    } else {
+        index = 0;
+        loadContent(index);
+    }
+};
+
+left.addEventListener('click', () => {
+    prev_content();
+});
+
+//next content
+function next_content() {
+    if (index < ImgList.length - 1) {
+        index += 1;
+        loadContent(index);
+    } else {
+        index = 0;
+        loadContent(index);
+    }
+};
+
+right.addEventListener('click', () => {
+    next_content();
+});
